f1e56033d0722f6bff4c9a7eded3ee86ba3748f8
Seth NeJame <seth@worldfavor.com>
2020-03-15 13:37:03 +0100

    Adding Burgers

---
+++ BerniesBurgers/BerniesBurgers/Pages/Burgers/List.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BerniesBurgers.Pages.Burgers
+{
+    public class List : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
