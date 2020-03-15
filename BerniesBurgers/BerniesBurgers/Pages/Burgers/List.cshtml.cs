e015f49b26394ad1efa22ced0e6140ef69d9936a
Seth NeJame <seth@worldfavor.com>
2020-03-15 19:42:30 +0100

    Adding burgers to burger page

--- BerniesBurgers/BerniesBurgers/Pages/Burgers/List.cshtml.cs
+++ BerniesBurgers/BerniesBurgers/Pages/Burgers/List.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using BerniesBurgers.Core;
+using BerniesBurgers.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 
 namespace BerniesBurgers.Pages.Burgers
@@ -6,16 +9,20 @@ namespace BerniesBurgers.Pages.Burgers
     public class ListModel : PageModel
     {
         public string Message { get; set; }
+        public IEnumerable<Burger> Burgers { get; set; }
 
         private readonly IConfiguration config;
+        private readonly IBurgerData burgerData;
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IBurgerData burgerData)
         {
+            this.burgerData = burgerData;
             this.config = config;
         }
         public void OnGet()
         {
             Message = config["Message"];
+            Burgers = burgerData.GetAll();
         }
     }
 }
